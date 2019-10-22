77a692eabf696856c529bb24b7c779e08cb950fd
Batmams <mamadou.koita@gmail.com>
2019-10-22 02:24:47 +0200

    Second commit

--- lib/controller.rb
+++ lib/controller.rb
@@ -1,19 +1,26 @@
-# require 'bundler'
-# require 'sinatra'
 require 'gossip'
 
 class ApplicationController < Sinatra::Base
 
   get '/' do
-    erb :index
-  end
+  erb :index, locals: {gossips: Gossip.all}
+end
 
   get '/gossips/new' do
-    erb :new_gossip
+  erb :new_gossip
   end
 
   post '/gossips/new/' do
-    Gossip.new(les_entrées_du_gossip).save
+    Gossip.new(#params["gossip_author"], #params["gossip_content"]).save
+    redirect '/'
+    end
+
+  get '/gossips/:number' do
+  erb :show, locals: {gossips_specific: Gossip, number: params['number']}
   end
 
+  get '/gossips/:number/edit' do
+  erb :show, locals: {gossips_specific: Gossip, number: params['number']}
+  end
+  end
 end
--- lib/gossip.rb
+++ lib/gossip.rb
@@ -1,8 +1,39 @@
-
+require 'csv'
 class Gossip
+    attr_accessor :content, :author
+
+    def initialize(author, content)
+        @content = content
+        @author = author
+    end
 
     def save
-        
+        CSV.open("./db/gossip.csv", "ab") do |csv|
+            csv << [@author, @content]
     end
 
 end
+
+def self.all
+    all_gossips = []
+    CSV.read("db/db.csv").each do |csv_line|
+        all_gossips << Gossip.new(csv_line[0], csv_line[1])
+    end
+
+    return all_gossips
+end
+
+def self.find(id)
+return Gossip.all[id.to_i]
+end
+
+def self.update(new_content)
+    self.content = new_content
+end
+end
+
+Gossip.new("Mams", "contenue du message").save
+Gossip.new("Mams", "contenue du message").save
+Gossip.new("Mams", "contenue du message").save
+Gossip.new("Mams", "contenue du message").save
+Gossip.new("Mams", "contenue du message").save
